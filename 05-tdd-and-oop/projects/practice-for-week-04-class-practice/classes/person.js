d5806e126c0825c6b3ec68f5e271e5e61174110a
Jacob <jacobgeorge08@gmail.com>
2023-02-03 11:24:26 +0530

    implemented the Person class and its methods

---
+++ 05-tdd-and-oop/projects/practice-for-week-04-class-practice/classes/person.js
@@ -0,0 +1,38 @@
+// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(arr) {
+
+    if (!Array.isArray(arr)) {
+      throw new Error("introducePeople only takes an array as an argument.")
+    }
+
+    arr.forEach(person => {
+      if (!(person instanceof Person)) {
+        throw new Error(`All items in array must be Person class instances.`)
+      }
+    })
+
+    arr.forEach(person => {
+      person.introduce();
+    })
+
+  }
+}
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
